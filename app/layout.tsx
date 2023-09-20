2356a51b72f782a7857f85609bdf3ad7c7dea9ff
jsaique <jsaique11@gmail.com>
2023-09-19 22:23:01 -0400

    Nav

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
+import Header from "@/components/Header";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "John Aries Saique | Portfolio",
-  description: "John Aries is a Software Developer",
+  title: "John Aries Saique | Portfolio V2",
+  description: "John Aries is a Self-Taught Software Developer",
 };
 
 export default function RootLayout({
@@ -15,7 +16,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${inter.className} bg-gray-50 text-gray-950 relative h-[5000px]`}
+      >
+        <div className="bg-[#fbe2e3] relative top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
+        <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
