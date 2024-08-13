fc2941474a25eb9c9ae9ea012d2e14b768880773
jsaique <jsaique11@gmail.com>
2024-08-13 17:05:27 -0400

    Added Firebase

--- lib/data.ts
+++ lib/data.ts
@@ -75,7 +75,7 @@ export const projectsData = [
   {
     title: "TechHub",
     description: "Simulates a real life ecommerce website",
-    tags: ["React", "Node.js", "TypeScript"],
+    tags: ["React", "Node.js", "TypeScript", "Firebase"],
     imageUrl: techhub,
     url: "https://tech-hub-backend-private-front.vercel.app",
   },
